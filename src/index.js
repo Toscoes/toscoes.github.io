27d3a3a3a400fe02abd33b1c5e9d1bec51535982
J. Yuen <jyuen0509@gmail.com>
2022-01-11 11:54:38 -0500

    add snowfall

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 /* Hi, if you're reading this: I know this is quite, quite messy :( 
    I really wanted to get this in for the application
 */
-let img1 = "./images/pong.png"
-let img2 = "./images/dd.png"
-let img3 = "./images/solarsystem.png"
-const Images = [img1,img2,img3]
-const Titles = ["", "Pong", "Dashing Dungeoneers", "CSS Solar System" ]
-const Hrefs = ["", "https://toscoes-socket-pong.herokuapp.com/", "./src/dashing_dungeoneers/index.html", "./src/cssss/solarsystem.html"]
-const Source = []
 
-const Descriptions = [
-    "",
-
-    `A clone of pong made using JavaScript, Node, and socket.io. 
-    Create a game, send the game code to a friend, and be the 
-    first to score 7 points! Oh, and you can also 'dash' up and down 
-    by pressing the spacebar, for a bit of spice.`,
-
-    `Slash through endless enemies in this 1-button, fast paced game. 
-    Made in pure JavaScript, my own programmer art, and inspired by an old flash game. 
-    Try to fake out the big guys with shields!`,
-
-    `A depiction of our solar system. No JavaScript, just HTML and CSS.`
-    
+const Content = [
+    {
+        image: "./images/pong.png",
+        title: "Pong",
+        description:`A clone of pong made using JavaScript, Node, and socket.io. 
+        Create a game, send the game code to a friend, and be the 
+        first to score 7 points! Oh, and you can also 'dash' up and down 
+        by pressing the spacebar, for a bit of spice.`,
+        link: "https://toscoes-socket-pong.herokuapp.com/",
+        src: "https://github.com/Toscoes/socket-pong"
+    },
+    {
+        image: "./images/dd.png",
+        title: "Dashing Dungeoneers",
+        description:`Slash through endless enemies in this 1-button, fast paced game. 
+        Made in pure JavaScript, my own programmer art, and inspired by an old flash game. 
+        Try to fake out the big guys with shields!.`,
+        link: "./src/dashing_dungeoneers/index.html",
+        src: "https://github.com/Toscoes/toscoes.github.io/tree/master/src/dashing_dungeoneers"
+    },
+    {
+        image: "./images/solarsystem.png",
+        title: "CSS Solar System",
+        description:`A depiction of our solar system. No JavaScript, just HTML and CSS.`,
+        link: "./src/cssss/solarsystem.html",
+        src: "https://github.com/Toscoes/toscoes.github.io/blob/master/src/cssss/solarsystem.html"
+    },
+    {
+        image: "./images/snowfall.png",
+        title: "Falling Snow",
+        description:`A pleasantly relaxing page of uniquely generated snowflakes.`,
+        link: "./src/snowfall/index.html",
+        src: "https://github.com/Toscoes/toscoes.github.io/tree/master/src/snowfall"
+    }
 ]
 
 const Carousel = document.getElementById("images")
@@ -30,14 +43,15 @@ const DescriptionCarousel = document.getElementById("descriptions")
 const btnPrev = document.getElementById("prev")
 const btnNext = document.getElementById("next")
 const Blips = document.getElementById("blips")
+let intervalId = null
 
 function init() {
     let startingIndex = -1
     let currentIndex = -startingIndex
-    Images.forEach(img => {
+    Content.forEach(e => {
         let item = document.createElement("div")
         item.classList.add("carousel-item")
-        item.style.backgroundImage = `url(${img})`
+        item.style.backgroundImage = `url(${e.image})`
         item.style.left = `${currentIndex * 100}%`
         
         Carousel.appendChild(item)
@@ -48,9 +62,9 @@ function init() {
         descriptionContainer.style.right = `${currentIndex * -120}%`
 
         descriptionContainer.innerHTML = `
-        <span class="bigger title text">${Titles[currentIndex]}</span>&nbsp;&nbsp;&nbsp;<span class="card desc text">${Descriptions[currentIndex]}</span></span>
+        <span class="bigger title text">${e.title}</span>&nbsp;&nbsp;&nbsp;<span class="card desc text">${e.description}</span></span>
         <div class="links desc text">
-            <a class="title text btn" href="${Hrefs[currentIndex]}">View</a><a class="title text btn" href="#">Source</a>
+            <a class="title text btn" href="${e.link}">View</a><a class="title text btn" href="${e.src}">Source</a>
         </div>
     `
 
@@ -70,20 +84,31 @@ function init() {
     })
 
     btnPrev.addEventListener("click", function() {
+        resetInterval()
         next(-1)
     })
 
     btnNext.addEventListener("click", function() {
+        resetInterval()
         next(1)
     })
 
+    function resetInterval() {
+        if (intervalId) {
+            clearInterval(intervalId)
+            intervalId = setInterval(()=> {
+                next(1)
+            }, 5000)
+        }
+    }
+
     function next(direction) {
         startingIndex += direction
-        if (startingIndex >= Images.length) {
+        if (startingIndex >= Content.length) {
             startingIndex = 0
         }
         if (startingIndex < 0) {
-            startingIndex = Images.length - 1
+            startingIndex = Content.length - 1
         }
         jump(startingIndex)
     }
@@ -115,7 +140,12 @@ function init() {
 
     setTimeout(()=> {
         jump(0)
+        intervalId = setInterval(()=> {
+            next(1)
+        }, 5000)
     }, 750)
+
+
 }
 
 init()
